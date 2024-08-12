f8e27ea57ae87d412090956300b1d03cf170e229
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-08-12 23:43:29 +0000

    entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
-import React from "react";
+
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	let [rojo, setRojo] = useState("bg-transparent");
+	let [ambar, setAmbar] = useState("bg-transparent")
+	let [verde, setVerde] = useState("bg-transparent")
+
+	function Parar() {
+		if(rojo==="bg-danger"){
+		setRojo("bg-transparent");
+		setAmbar("bg-transparent");
+		setVerde("bg-transparent");
+		}
+	else
+		setRojo("bg-danger");
+		setAmbar("bg-transparent");
+		setVerde("bg-transparent");
+	}
+	
+	function Esperar() {
+		if(ambar==="bg-warning"){
+		setRojo("bg-transparent");
+		setAmbar("bg-transparent");
+		setVerde("bg-transparent");
+		}
+	else
+		setAmbar("bg-warning");
+		setRojo("bg-transparent");
+		setVerde("bg-transparent");
+	}
+
+	function Avanzar() {
+		if(verde==="bg-success"){
+		setRojo("bg-transparent");
+		setAmbar("bg-transparent");
+		setVerde("bg-transparent");
+		}
+	else
+		setVerde("bg-success");
+		setRojo("bg-transparent");
+		setAmbar("bg-transparent");
+	}
+	
+
+
+
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center align-items-center justify-content-center">
+			<div className="container text-center align-items-center justify-content-center">
+				<div className="row bg-dark text-center justify-content-center py-2" id="contenedor">
+					<div className="row text-center justify-content-center">
+						<button onClick={Parar} className={`rounded-circle border-dark ${rojo}`}></button>
+					</div>
+					<div className="row">
+						<button onClick={Esperar} className={`rounded-circle ${ambar}`}></button>
+					</div>
+					<div className="row">
+						<button onClick={Avanzar} className={`rounded-circle ${verde}`}></button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
